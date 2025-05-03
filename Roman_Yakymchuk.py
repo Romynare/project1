2e92709e7d1a547fcadead0c1e55c1584aaa0e7b
Romynare <yakymchuk.roman2010@gmail.com>
2025-05-03 11:54:26 +0300

    Update Roman_Yakymchuk.py

--- Roman_Yakymchuk.py
+++ Roman_Yakymchuk.py
@@ -36,17 +36,7 @@ def gameLoop():
     length_of_snake = 1
     foodx = round(random.randrange(0, dis_width - snake_block)/ 10.0) * 10.0
     foody = round(random.randrange(0, dis_height - snake_block)/ 10.0) * 10.0
-    while not game_over:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change =-snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
+   
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -59,11 +49,11 @@ def gameLoop():
                     x1_change = snake_block
                     y1_change = 0
                 elif event.key == pygame.K_UP:
-                    x1_change = -snake_block
-                    y1_change = 0
+                    y1_change = -snake_block
+                    x1_change = 0
                 elif event.key == pygame.K_DOWN:
-                    x1_change = snake_block
-                    y1_change = 0
+                    y1_change = snake_block
+                    x1_change = 0
         x1 += x1_change
         y1 += y1_change
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
@@ -83,18 +73,19 @@ def gameLoop():
             our_snake(snake_block, snake_list)
             Your_score(length_of_snake - 1)
             pygame.display.update()
-            if x1 == foodx and foody:
-                foodx = round(random,randrange(0,dis_width - snake_block))
-                foody = round(random.randrange(0, dis_height - snake_block))
+            if x1 == foodx and y1 == foody:
+                foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+                foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
                 length_of_snake += 1
             clock.tick(snake_speed)
-    fill(light_red)
-    massage ("You Lost!Your Score: "+  str(length_of_snake - 1),black)
+    dis.fill(light_red)
+    message("You Lost! Your Score: " + str(length_of_snake - 1), black)
+    time.sleep(2)
     pygame.display.update()
-    sleep(2)
+
 while True:
     try:
         gameLoop()
     except Exception as e:
         pygame.quit()
-        break
+        quit()
